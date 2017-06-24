21e3098f6079f2a91d6ea2670ab3183777195ea8
Zach Lipps <zacharylipps10@gmail.com>
2017-06-24 15:49:06 -0700

    render login

--- App.js
+++ App.js
@@ -2,14 +2,19 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Landing from './src/components/Landing';
 import { StackNavigator } from 'react-navigation';
-import {Tabs} from './src/routes'
+import { Tabs } from './src/routes';
 
-export default class App extends React.Component{
-  render(){
-    return <Tabs/>
+export default class App extends React.Component {
+  render() {
+    return (
+      <View>
+        <Tabs />
+        <Landing />
+      </View>
+
+    );
   }
 }
-
 
 
 const styles = StyleSheet.create({
