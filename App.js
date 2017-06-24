e2605bfaf08d015b559bb9cfda9e113949950053
Zach Lipps <zacharylipps10@gmail.com>
2017-06-24 15:32:40 -0700

    resolve conflicts

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Landing from './src/components/Landing';
 import { StackNavigator } from 'react-navigation';
-
 import {Tabs} from './src/routes'
 
 export default class App extends React.Component{
